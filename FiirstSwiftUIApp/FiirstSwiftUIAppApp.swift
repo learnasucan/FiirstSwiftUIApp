f29fa8e91719df0b9509ab3406176eb70d684d49
Prachit <prachit.p@codewalla.com>
2023-05-03 19:03:58 +0530

    Initial Commit

---
+++ FiirstSwiftUIApp/FiirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiirstSwiftUIAppApp.swift
+//  FiirstSwiftUIApp
+//
+//  Created by Prachit on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FiirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
